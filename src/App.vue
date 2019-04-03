4d8f9abe59e55d7a4a581aaef212d786037e808a
pablovester <vester@pablovester.com.ar>
2019-04-03 02:19:57 -0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <div id="app">
+    <Bar/>
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  </div>
+</template>
+
+<script>
+import Bar from './components/Bar.vue'
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'app',
+  components: {
+    Bar,
+    HelloWorld
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Montserrat', sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin: 0;
+  background-color: black;
+}
+
+</style>
---
+++ src/components/Bar.vue
@@ -0,0 +1,35 @@
+<template>
+  <div id="bar">
+  	<div id="btn">{{ btntxt }}DOWNLOAD</div>
+  </div>
+</template>
+
+<script>
+data: {
+  btntxt: "boke"
+}
+</script>
+
+<style>
+#bar {
+  height: 20px;
+  width: 100%;
+  background-color: rgb(21, 22, 25);
+  height: 60px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+
+#btn {
+  border-radius: 10px;
+  background-color: rgb(58, 190, 239);
+  width: 160px;
+  height: 40px;
+  color: white;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  font-weight: 800;
+}
+</style>
